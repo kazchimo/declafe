54e63419899b91c7682aea29066f3bc5d7bd50c2
kazuki <pharcydetip@gmail.com>
2022-07-14 11:36:03 +0900

    add log test
    add log test case

--- tests/feature_gen/test_feature_gen.py
+++ tests/feature_gen/test_feature_gen.py
@@ -1,23 +1,56 @@
 import pandas as pd
 
-from declafe import FeatureGen
+from declafe import FeatureGen, LogFeature
+from declafe.dsl import c
+
+test_df = pd.DataFrame(
+    {
+        "a": list(range(1, 1001)),
+        "b": list(range(1001, 2001))
+    })
+
+
+class SimpleGen(FeatureGen):
+
+  def gen(self, df: pd.DataFrame) -> pd.Series:
+    return pd.Series(1, index=df.index)
+
+  def _feature_name(self) -> str:
+    return "test_gen"
+
+
+_1 = c(1)
+
+
+class Double(FeatureGen):
+
+  def __init__(self, column: str):
+    super().__init__()
+    self.column = column
+
+  def gen(self, df: pd.DataFrame) -> pd.Series:
+    return df[self.column] * 2
+
+  def _feature_name(self) -> str:
+    return "double"
 
 
 class TestFeatureName:
 
-  class SimpleGen(FeatureGen):
-
-    def gen(self, df: pd.DataFrame) -> pd.Series:
-      return pd.Series("test", index=df.index)
-
-    def _feature_name(self) -> str:
-      return "test_gen"
-
   def test_return_pre_defined_name_if_not_overrode(self):
-    gen = self.SimpleGen()
+    gen = SimpleGen()
     assert gen.feature_name == "test_gen"
 
   def test_return_overrode_name(self):
-    gen = self.SimpleGen()
+    gen = SimpleGen()
     gen.as_name_of("overrode")
     assert gen.feature_name == "overrode"
+
+
+class TestLog:
+
+  def test_return_log(self):
+    assert _1.log().gen(test_df).equals(
+        LogFeature("").gen_unary(pd.Series(1, index=test_df.index)))
+    assert Double("a").log().gen(test_df).equals(
+        LogFeature("").gen_unary(test_df["a"] * 2))
