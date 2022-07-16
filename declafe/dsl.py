e0cfe584dfdcd9a22e512269d574b43a1ea71d29
kazuki <pharcydetip@gmail.com>
2022-07-16 12:50:28 +0900

    add cols dsl

--- declafe/dsl.py
+++ declafe/dsl.py
@@ -1,5 +1,6 @@
-from typing import Any, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING, List
 
+from . import Features
 from .ConstFeature import ConstFeature
 
 if TYPE_CHECKING:
@@ -13,3 +14,6 @@ def c(v: Any) -> ConstFeature:
 def col(column_name: str) -> "IdFeature":
   from .unary.IdFeature import IdFeature
   return IdFeature(column_name)
+
+def cols(column_names: List[str]) -> "Features":
+  return Features([col(co) for co in column_names])
