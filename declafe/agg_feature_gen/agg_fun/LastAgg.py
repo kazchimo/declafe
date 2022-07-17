758385eac7a2ae00d0a9cb529f1480386cec72a3
kazuki <pharcydetip@gmail.com>
2022-07-17 17:41:33 +0900

    optimized Last

--- declafe/agg_feature_gen/agg_fun/LastAgg.py
+++ declafe/agg_feature_gen/agg_fun/LastAgg.py
@@ -8,7 +8,7 @@ from .AggFun import AggFun
 class LastAgg(AggFun):
 
   def __call__(self, ser: pd.Series) -> Any:
-    return ser.iloc[-1]
+    return ser.iat[-1]
 
   @property
   def fun_name(self) -> str:
