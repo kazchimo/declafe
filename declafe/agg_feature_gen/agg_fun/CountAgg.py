6c2bf8231572d3b45fb15864cefa947e0b097117
kazuki <pharcydetip@gmail.com>
2022-07-17 17:38:26 +0900

    optimized count

--- declafe/agg_feature_gen/agg_fun/CountAgg.py
+++ declafe/agg_feature_gen/agg_fun/CountAgg.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from .AggFun import AggFun
@@ -7,7 +6,7 @@ from .AggFun import AggFun
 class CountAgg(AggFun):
 
   def __call__(self, ser: pd.Series) -> int:
-    return np.size(ser)
+    return ser.size
 
   @property
   def fun_name(self) -> str:
