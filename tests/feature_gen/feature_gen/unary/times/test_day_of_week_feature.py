dde9823bf9219a3786f37da1ae27f2487da29900
kazuki <pharcydetip@gmail.com>
2022-09-14 19:01:46 +0900

    remove dataclass

---
+++ tests/feature_gen/feature_gen/unary/times/test_day_of_week_feature.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+import pandas as pd
+
+from declafe.feature_gen.unary.times import DayOfWeekFeature
+
+
+class TestGen:
+  def test_convert_to_day_of_week(self):
+    df = pd.DataFrame({"date": [datetime(2020, 1, 1), datetime(2020, 1, 2)]})
+    assert DayOfWeekFeature("date").gen(df).equals(pd.Series([2, 3]))
