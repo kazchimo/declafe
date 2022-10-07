5b154d254b9ea3ce57d029f6b6709c9462fbbd1b
kazuki <pharcydetip@gmail.com>
2022-10-07 20:53:24 +0900

    version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-VERSION = "0.0.20"
+VERSION = "0.0.21"
 
 package_name = "declafe"
 root_dir = path.abspath(path.dirname(__file__))
