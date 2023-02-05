9f685002d4121aa3db23142d83c7f47f76def47e
kazuki <pharcydetip@gmail.com>
2023-02-05 15:23:16 +0900

    add gt

--- pl/feature_gen/feature_gen.py
+++ pl/feature_gen/feature_gen.py
@@ -221,6 +221,10 @@ class FeatureGen(ABC):
     from pl.feature_gen.binary.ops.ge_feature import GEFeature
     return GEFeature(self, fg.conv_lit(other))
 
+  def __gt__(self, other: O) -> "FeatureGen":
+    from pl.feature_gen.binary.ops.gt_feature import GTFeature
+    return GTFeature(self, fg.conv_lit(other))
+
   def __invert__(self) -> "FeatureGen":
     from pl.feature_gen.unary.invert_feature import InvertFeature
     return InvertFeature(self)
---
+++ tests/pl/feature_gen/binary/ops/test_gt_feature.py
@@ -0,0 +1,27 @@
+import polars as pl
+import pl.feature_gen as fg
+
+
+def test_gt_feature():
+  df = pl.DataFrame({"a": [1, 2, 3, 4, 5, 6], "b": [1, 2, -3, 4, 5, 6]})
+
+  gt = fg.col("a") > fg.col("b")
+  assert gt(df).series_equal(
+      pl.Series(
+          "a_>_b",
+          [False, False, True, False, False, False],
+      ))
+
+  gt_const = fg.col("a") > 2
+  assert gt_const(df).series_equal(
+      pl.Series(
+          "a_>_2",
+          [False, False, True, True, True, True],
+      ))
+
+  gt_lit = fg.col("a") > fg.lit(2)
+  assert gt_lit(df).series_equal(
+      pl.Series(
+          "a_>_2",
+          [False, False, True, True, True, True],
+      ))
