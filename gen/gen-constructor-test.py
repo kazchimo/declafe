c499e09478c2d22e862e646014ab5b142e371527
kazuki <pharcydetip@gmail.com>
2023-02-10 00:25:26 +0900

    gen talib constructor test

---
+++ gen/gen-constructor-test.py
@@ -0,0 +1,77 @@
+import re
+
+from gen.services.talib_constructor_def import TalibConstructorDef
+
+
+class TalibConstructorTests:
+
+  def __init__(self, chain_defs: list[TalibConstructorDef]):
+    self.chain_defs = chain_defs
+
+  @property
+  def file_content(self) -> str:
+    tests = '\n'.join(
+        [self._test_content(chain_def) for chain_def in self.chain_defs])
+
+    return f"""\
+import talib
+import polars as pl
+import pl.feature_gen as fg
+import numpy as np
+
+df = pl.DataFrame({{
+      "close": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      "open": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      "high": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      "low": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      "volume": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }})
+
+{tests}
+"""
+
+  def _test_content(self, chain_def: TalibConstructorDef) -> str:
+    idx_component = f"[{chain_def.talib_method_idx}]" if chain_def.talib_method_idx is not None else ""
+    arg_component = ", ".join([
+        arg.as_pass_component(arg.test_value())
+        for idx, arg in enumerate(chain_def.args)
+    ])
+    arg_names = ", ".join(
+        [a.test_value_name() for idx, a in enumerate(chain_def.additionl_args)])
+    df_arg_component = ", ".join([
+        arg.as_pass_component(arg.test_df_value())
+        for idx, arg in enumerate(chain_def.args)
+    ])
+    primary_args = ", ".join([a.name for a in chain_def.primary_args])
+
+    return f"""\
+class Test{chain_def.def_name.capitalize()}:
+  def test_{chain_def.def_name}(self):
+    gen = fg.talib.{chain_def.def_name}({arg_component})
+    name = "{chain_def.def_name.upper()}({arg_names})({primary_args})"
+    assert np.array_equal(gen(df).to_numpy(), 
+      talib.{chain_def.talib_method_name}({df_arg_component}){idx_component}.alias(name),
+      equal_nan=True
+    )
+"""
+
+  def write(self):
+    with open("tests/pl/feature_gen/test_talib_constructor.py", "w") as f:
+      f.write(self.file_content)
+
+
+def main():
+  def_regex = re.compile(r'def\s+(\w+)\((.*?)\) -> "FeatureGen":',
+                         re.MULTILINE | re.DOTALL)
+  with open("pl/feature_gen/talib_constructor.py", "r") as f:
+    chain_defs = [
+        TalibConstructorDef(l[0], l[1].replace(" ", "").replace("\n", ""))
+        for l in def_regex.findall("\n".join(f.read().split("\n")[8:]))
+    ]
+
+  chain_tests = TalibConstructorTests(chain_defs)
+  chain_tests.write()
+
+
+if __name__ == "__main__":
+  main()
---
+++ gen/services/talib_constructor_def.py
@@ -0,0 +1,101 @@
+import re
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Arg:
+  name: str
+  type: str
+
+  @staticmethod
+  def read(line: str) -> "Arg":
+    name, type = line.split(":")
+    return Arg(name.strip(), type.strip())
+
+  def as_pass_component(self, value: str) -> str:
+    if self.type == "ColLike":
+      return f"{value}"
+    else:
+      return f"{self.name}={value}"
+
+  def test_value_name(self) -> str:
+    if self.type == "ColLike":
+      return f'{self.name}'
+    else:
+      if "type" in self.name:
+        return "1"
+      elif "period" in self.name:
+        return "3"
+      elif "fastlimit" in self.name:
+        return "0.5"
+      elif "slowlimit" in self.name:
+        return "0.05"
+      else:
+        return "1"
+
+  def test_df_value(self) -> str:
+    if self.type == "ColLike":
+
+      return f'df["{self.name}"]'
+    else:
+      if "type" in self.name:
+        return "1"
+      elif "period" in self.name:
+        return "3"
+      elif "fastlimit" in self.name:
+        return "0.5"
+      elif "slowlimit" in self.name:
+        return "0.05"
+      else:
+        return "1"
+
+  def test_value(self) -> str:
+    if self.type == "ColLike":
+      return f'fg.col("{self.name}")'
+    else:
+      if "type" in self.name:
+        return "1"
+      elif "period" in self.name:
+        return "3"
+      elif "fastlimit" in self.name:
+        return "0.5"
+      elif "slowlimit" in self.name:
+        return "0.05"
+      else:
+        return "1"
+
+
+@dataclass
+class TalibConstructorDef:
+  def_name: str
+  _args: str
+
+  _talib_multi_multi_methods_regex = re.compile(r"(.*)_(\d)")
+
+  @property
+  def args(self) -> list[Arg]:
+    return [Arg.read(a) for a in self._args.split(",")[1:]]
+
+  @property
+  def additionl_args(self) -> list[Arg]:
+    return [a for a in self.args if a.type != "ColLike"]
+
+  @property
+  def primary_args(self) -> list[Arg]:
+    return [a for a in self.args if a.type == "ColLike"]
+
+  @property
+  def talib_method_name(self) -> str:
+    if self._talib_multi_multi_methods_regex.match(self.def_name):
+      return self._talib_multi_multi_methods_regex.match(
+          self.def_name).group(1).upper()
+    else:
+      return self.def_name.upper()
+
+  @property
+  def talib_method_idx(self) -> Optional[int]:
+    if self._talib_multi_multi_methods_regex.match(self.def_name):
+      return int(self.def_name[-1])
+    else:
+      return None
